71316e9d232c684f98b64c9917665601d0e5153d
KamogeloMafaesa <1693173@students.wits.ac.za>
2020-07-02 00:20:24 +0200

    Initial commit

---
+++ Index.js
@@ -0,0 +1,25 @@
+setInterval(setClock, 1000);
+
+const Hourhand = document.querySelector('[data-hour-hand]');
+const Minutehand = document.querySelector('[data-minute-hand]');
+const Secondhand = document.querySelector('[data-second-hand]');
+
+
+function setClock(){
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() /60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) /60
+    const hourssRatio = (minutesRatio + currentDate.getHours()) / 12
+
+
+    setRotation(Secondhand,secondsRatio);
+    setRotation(Minutehand,minutesRatio);
+    setRotation(Hourhand,hoursRatio);
+
+}
+
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+setClock()
